df9889ddc2936f1ece2ea6c19709ecaef4fe017d
Daniel Berezhnoy <43868541+Daniel-Berezhnoy@users.noreply.github.com>
2023-04-06 12:54:05 -0700

    Save

--- BasketballSim/Model/GameModel.swift
+++ BasketballSim/Model/GameModel.swift
@@ -8,25 +8,26 @@
 import Foundation
 
 final class GameModel: ObservableObject, GameSimulatorDelegate {
-
+    
     @Published var gameState = GameState(homeScore: 0,
-                                          awayScore: 0,
-                                          scoringTeamName: "",
-                                          lastAction: "")
+                                         awayScore: 0,
+                                         scoringTeamName: "",
+                                         lastAction: "")
     let simulator = GameSimulator()
-
+    
     init() {
         simulator.delegate = self
     }
-
+    
+    func startLiveActivity() {
+        
+    }
+    
     func didUpdate(gameState: GameState) {
         self.gameState = gameState
     }
-
+    
     func didCompleteGame() {
-
+        
     }
-
-
-    // Live Activity code goes here
 }
